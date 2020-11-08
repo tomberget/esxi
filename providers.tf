ac306ceb61d1c9e067ee762700d5804b37ff44be
Tom Stian Berget <tom.berget@tietoevry.com>
2020-11-08 15:56:29 +0100

    Update provider settings

--- providers.tf
+++ providers.tf
@@ -1,1 +1,20 @@
-provider "vsphere" {}
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "~> 1.24.2"
+    }
+  }
+}
+
+provider "vsphere" {
+  # user           = var.vsphere_user
+  # password       = var.vsphere_password
+  # vsphere_server = var.vsphere_server
+
+  # # If you have a self-signed cert
+  # allow_unverified_ssl = true
+
+  # Enable debugging
+  # client_debug = true
+}
