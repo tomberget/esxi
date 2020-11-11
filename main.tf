b8672f0041cedb56d60e7fb054c9ba36cff849de
Tom Stian Berget <tom.berget@tietoevry.com>
2020-11-11 01:03:43 +0100

    Add local-storage storage class

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
 resource "kubernetes_storage_class" "default" {
   metadata {
-    name = "fast"
+    name = "local-storage"
   }
-  storage_provisioner = "kubernetes.io/vsphere-volume"
-  reclaim_policy      = "Retain"
-  parameters = {
-    diskformat = "thin"
-    datastore  = "kubernetes"
-  }
-  mount_options = ["file_mode=0700", "dir_mode=0777", "mfsymlinks", "uid=1000", "gid=1000", "nobrl", "cache=none"]
+  storage_provisioner  = "kubernetes.io/no-provisioner"
+  reclaim_policy       = "Retain"
+  volume_binding_mode  = "WaitForFirstConsumer"
 }
