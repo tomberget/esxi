c298ec43ea31ec49db223b98d39a566ba005039d
Tom Stian Berget <tom.berget@tietoevry.com>
2021-08-29 01:10:30 +0200

    Add Traefik ingress route module

---
+++ modules/traefik_ingress_route/main.tf
@@ -0,0 +1,35 @@
+resource "kubernetes_manifest" "ingress_route" {
+  manifest = {
+    apiVersion = "traefik.containo.us/v1alpha1"
+    kind       = "IngressRoute"
+    metadata = {
+      name      = "${var.service_name}-ingressroute"
+      namespace = var.namespace
+    }
+
+    spec = {
+      entryPoints = [
+        "web",
+      ]
+      routes = [
+        {
+          kind  = var.route_kind
+          match = var.route_match
+          services = [
+            {
+              kind = var.service_kind
+              name = var.service_name
+              namespace = var.namespace
+              passHostHeader = var.service_pass_host_header
+              responseForwarding = {
+                flushInterval = var.service_responseForwarding_flushInterval
+              }
+              scheme = var.service_scheme
+              port = var.service_port
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
---
+++ modules/traefik_ingress_route/variables.tf
@@ -0,0 +1,56 @@
+variable "service_name" {
+  description = "Name used for service name"
+  type        = string
+}
+
+variable "namespace" {
+  description = "The namespace the IngressRoute will use"
+  type        = string
+}
+
+variable "route_kind" {
+  description = "Route kind. Most likely Rule"
+  type = string
+  default = "Rule"
+}
+
+variable "route_match" {
+  description = "Route matching"
+  type = string
+}
+
+variable "service_kind" {
+  description = "Kind of service"
+  type = string
+  default = "Service"
+}
+
+variable "service_pass_host_header" {
+  description = "Pass host header"
+  type = bool
+  default = true
+}
+
+variable "service_scheme" {
+  description = "Scheme used"
+  type = string
+  default = "http"
+}
+
+variable "service_port" {
+  description = "Named port, or port number"
+  type = string
+  default = "http"
+}
+
+variable "service_responseForwarding_flushInterval" {
+  description = "The flush interval of the response forwarding"
+  type = string
+  default = "1ms"
+}
+
+variable "service_strategy" {
+  description = "Type of strategy for load balancing"
+  type = string
+  default = "RoundRobin"
+}
