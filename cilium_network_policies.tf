d8c14dee2260cd115454defa49c6d24ac3d60db3
Tom Stian Berget <tom.berget@tietoevry.com>
2021-11-29 10:51:06 +0100

    Bump cilium network policies (#8)
    Co-authored-by: Tom Stian Berget <tom.berget@gmail.com>

--- cilium_network_policies.tf
+++ cilium_network_policies.tf
@@ -1,5 +1,5 @@
 module "cilium_network_policy" {
-  source = "github.com/evry-ace/tf-cilium-network-policies.git?ref=v1.0.2"
+  source = "github.com/evry-ace/tf-cilium-network-policies.git?ref=v1.0.7"
 
   enable_dns_visibility = true
 
--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.0"
+      version = "~> 2.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0"
+      version = "~> 2.0"
     }
     local = {
       source  = "hashicorp/local"
