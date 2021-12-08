f68ea65759c6478512992e101be564dc3d9a9886
Tom Stian Berget <tom.berget@tietoevry.com>
2021-12-08 23:13:10 +0100

    Upgrades, breaking PiHole (#10)
    * Upgrades, breaking PiHole
    
    * Fixed pihole, but no persisted volume

--- modules/pihole/pihole.tf
+++ modules/pihole/pihole.tf
@@ -28,35 +28,6 @@ resource "kubernetes_secret" "pihole" {
   type = "opaque"
 }
 
-resource "kubernetes_persistent_volume" "pihole" {
-  metadata {
-    name = "pihole-local-storage-pv"
-  }
-  spec {
-    capacity = {
-      storage = "500Mi"
-    }
-    access_modes       = ["ReadWriteOnce"]
-    storage_class_name = "local-storage"
-    persistent_volume_source {
-      local {
-        path = "/mnt/kubeshare/pihole"
-      }
-    }
-    node_affinity {
-      required {
-        node_selector_term {
-          match_expressions {
-            key      = "kubernetes.io/hostname"
-            operator = "In"
-            values   = ["k8node1", "k8node2"]
-          }
-        }
-      }
-    }
-  }
-}
-
 resource "helm_release" "pihole" {
   name       = var.chart_name
   namespace  = kubernetes_namespace.pihole.metadata[0].name
@@ -68,8 +39,8 @@ resource "helm_release" "pihole" {
     templatefile("${path.module}/templates/values.yaml", {
       secret_name       = kubernetes_secret.pihole.metadata[0].name
       metallb_pihole_ip = var.metallb_pihole_ip
+      ingress_host      = "${var.chart_name}.${var.domain}"
+      tls_secret        = "${var.chart_name}-${replace(var.domain, ".", "-")}-tls"
     })
   ]
-
-  depends_on = [kubernetes_persistent_volume.pihole]
 }
