22e521d3e13aeef510fd63e576baaae15edb1e46
Tom Stian Berget <toms.berget@gmail.com>
2021-12-08 23:12:37 +0100

    Fixed pihole, but no persisted volume

--- modules/pihole/pihole.tf
+++ modules/pihole/pihole.tf
@@ -28,35 +28,6 @@ resource "kubernetes_secret" "pihole" {
   type = "opaque"
 }
 
-resource "kubernetes_persistent_volume" "pihole" {
-  metadata {
-    name = "pihole-local-storage-pv"
-  }
-  spec {
-    capacity = {
-      storage = "500Mi"
-    }
-    access_modes       = ["ReadWriteOnce"]
-    storage_class_name = "local-storage"
-    persistent_volume_source {
-      local {
-        path = "/mnt/kubeshare/pihole"
-      }
-    }
-    node_affinity {
-      required {
-        node_selector_term {
-          match_expressions {
-            key      = "kubernetes.io/hostname"
-            operator = "In"
-            values   = ["k8node1", "k8node2"]
-          }
-        }
-      }
-    }
-  }
-}
-
 resource "helm_release" "pihole" {
   name       = var.chart_name
   namespace  = kubernetes_namespace.pihole.metadata[0].name
@@ -72,6 +43,4 @@ resource "helm_release" "pihole" {
       tls_secret        = "${var.chart_name}-${replace(var.domain, ".", "-")}-tls"
     })
   ]
-
-  depends_on = [kubernetes_persistent_volume.pihole]
 }
