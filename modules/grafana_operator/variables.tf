7711d6faaaf17975b00ffbf915484911ec0751eb
Tom Stian Berget <tom.berget@gmail.com>
2021-12-11 00:27:36 +0100

    files

---
+++ modules/grafana_operator/variables.tf
@@ -0,0 +1,45 @@
+variable "grafana_operator_name" {
+  type        = string
+  default     = "grafana-operator"
+  description = "Name of the helm release"
+}
+
+variable "grafana_operator_namespace" {
+  type        = string
+  description = "It specify the grafana-operator namespace where the grafana-operator will deployed"
+}
+
+variable "grafana_operator_chart_version" {
+  type        = string
+  description = "This will specify the grafana-operator chart version"
+}
+
+variable "grafana_operator_chart_repository" {
+  type        = string
+  description = "It specifies the chart repository of grafana-operator chart"
+}
+
+variable "grafana_ingress_host" {
+  type        = string
+  description = "This will specify the ingress url"
+}
+
+variable "grafana_data_source_url" {
+  type        = string
+  description = "This will specify prometheus datasource url for prod"
+}
+
+variable "grafana_data_source_url_alertmanager" {
+  description = "This will specify the Alertmanager datasource url for prod"
+  type        = string
+}
+
+variable "grafana_data_source_access" {
+  type        = string
+  description = "This will specify prometheus datasource url"
+}
+
+variable "grafana_labels" {
+  description = "Add labels to service, pod and ingress."
+  type        = map(string)
+}
