5ece73f6060594844f64d0b9bfe38f30c04f888a
Tom Stian Berget <toms.berget@gmail.com>
2021-12-11 01:37:52 +0100

    Add namespaces file

---
+++ namespaces.tf
@@ -0,0 +1,42 @@
+
+resource "kubernetes_namespace" "cert_manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "kubernetes_namespace" "home_assistant" {
+  metadata {
+    name = "home-assistant"
+  }
+}
+
+resource "kubernetes_namespace" "metallb" {
+  metadata {
+    name = "metallb"
+  }
+}
+
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+resource "kubernetes_namespace" "nginx" {
+  metadata {
+    name = "nginx"
+  }
+}
+
+resource "kubernetes_namespace" "traefik" {
+  metadata {
+    name = "traefik"
+  }
+}
+
+resource "kubernetes_namespace" "pihole" {
+  metadata {
+    name = "pihole"
+  }
+}
