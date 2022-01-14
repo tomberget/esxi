2b03819281f8bff7f0b037f7a74c9c5293725afc
Tom Stian Berget <toms.berget@gmail.com>
2022-01-14 01:06:01 +0100

    Fix persistence with NFS
    Fix persistence by defining the correct NFS values

--- modules/pihole/pihole.tf
+++ modules/pihole/pihole.tf
@@ -17,6 +17,45 @@ resource "kubernetes_secret" "pihole" {
   type = "opaque"
 }
 
+resource "kubernetes_persistent_volume" "pihole" {
+  count = var.enable_persistent_volume ? 1 : 0
+
+  metadata {
+    name = "pihole-local-storage-pv"
+  }
+  spec {
+    capacity = {
+      storage = "500Mi"
+    }
+    access_modes       = ["ReadWriteOnce"]
+    storage_class_name = "local-storage"
+    mount_options = [
+      "nfsvers=3",
+      "noatime",
+      "nolock",
+      "all_squash",
+      "anonuid=1000",
+      "anongid=65533",
+    ]
+    persistent_volume_source {
+      local {
+        path = "/mnt/kubeshare/pihole"
+      }
+    }
+    node_affinity {
+      required {
+        node_selector_term {
+          match_expressions {
+            key      = "kubernetes.io/hostname"
+            operator = "In"
+            values   = ["k8node1", "k8node2"]
+          }
+        }
+      }
+    }
+  }
+}
+
 resource "helm_release" "pihole" {
   name       = var.chart_name
   namespace  = var.namespace
@@ -26,10 +65,13 @@ resource "helm_release" "pihole" {
 
   values = [
     templatefile("${path.module}/templates/values.yaml", {
-      secret_name       = kubernetes_secret.pihole.metadata[0].name
-      metallb_pihole_ip = var.metallb_pihole_ip
-      ingress_host      = "${var.chart_name}.${var.domain}"
-      tls_secret        = "${var.chart_name}-${replace(var.domain, ".", "-")}-tls"
+      secret_name              = kubernetes_secret.pihole.metadata[0].name
+      metallb_pihole_ip        = var.metallb_pihole_ip
+      ingress_host             = "${var.chart_name}.${var.domain}"
+      tls_secret               = "${var.chart_name}-${replace(var.domain, ".", "-")}-tls"
+      enable_persistent_volume = var.enable_persistent_volume
     })
   ]
+
+  depends_on = [kubernetes_persistent_volume.pihole]
 }
--- modules/pihole/variables.tf
+++ modules/pihole/variables.tf
@@ -1,7 +1,16 @@
 variable "namespace" {}
+
 variable "chart_version" {}
+
 variable "domain" {}
+
 variable "chart_name" {}
+
 variable "metallb_pihole_ip" {
   type = string
+}
+
+variable "enable_persistent_volume" {
+  type    = bool
+  default = true
 }
